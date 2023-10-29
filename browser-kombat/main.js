46b1ce5ba45e617a82e2f318f9cbb6f45caffe0e
baloghbe <92228708+baloghbe@users.noreply.github.com>
2023-10-29 17:50:29 +0100

    Healthbars + hit logic added

--- browser-kombat/main.js
+++ browser-kombat/main.js
@@ -3,6 +3,7 @@ kaboom({
     width: 1280,
     height: 720,
     scale: 1,
+    debug:false
 })
 //World texture initialization
 //World sprites, animations
@@ -441,12 +442,14 @@ scene("fight", () =>{
         anchor("center")
     ])
     const count = counter.add([
+        
         text("60"),
         area(),
         anchor("center"),
         {
             timeleft: 60,
-        }
+        },
+        
     ])
 
     const winningText = add([
@@ -464,7 +467,7 @@ scene("fight", () =>{
             winningText.text = "Tie!"
         }else if (player1.health > 0 && player2.health ===0){
             winningText.text = "Player 1 won!"
-            player.use(sprite(player2.sprites.death))
+            player2.use(sprite(player2.sprites.death))
             player2.play("death")
         }else{
             winningText.text = "Player 2 won!"
@@ -484,6 +487,77 @@ scene("fight", () =>{
         count.timeLeft--
         count.text = count.timeLeft
     }, 1000)
+
+    const player1HealthContainer = add([
+        rect(500,70),
+        area(),
+        outline(5),
+        pos(90,20),
+        color(200,0,0)
+    ])
+
+    const player1HealthBar = player1HealthContainer.add([
+        rect(498,65),
+        color(0,100,0),
+        pos(498,70 -2.5),
+        rotate(180)
+    ])
+
+    player1.onCollide(player2.id + "attackHitbox", ()=>{
+        if(gameOver){
+            return
+        }
+
+        if(player1.health !==0){
+            player1.health -=50
+            tween(player1HealthBar.width, player1.health, 1, (val)=>{
+                player1HealthBar.width = val
+            }, easings.easeOutSine)
+        }
+
+        if(player1.health === 0){
+            clearInterval(countInterval)
+            declareWinner(winningText, player1, player2)
+            gameOver = true
+        }
+
+
+    })
+
+    const player2HealthContainer = add([
+        rect(500, 70),
+        area(),
+        outline(5),
+        pos(690, 20),
+        color(200,0,0)
+    ])
+       
+    const player2HealthBar = player2HealthContainer.add([
+        rect(498, 65),
+        color(0,180,0),
+        pos(2.5, 2.5),
+    ])
+    
+    player2.onCollide(player1.id + "attackHitbox", () => {
+        if (gameOver) {
+            return
+        }
+        
+        if (player2.health !== 0) {
+            player2.health -= 50 
+            tween(player2HealthBar.width, player2.health, 1, (val) => {
+                player2HealthBar.width = val
+            }, easings.easeOutSine) 
+        } 
+        
+        if (player2.health === 0) {
+            clearInterval(countInterval)
+            declareWinner(winningText, player1, player2)
+            gameOver = true
+        }
+    })
+
+    
 })
 
 go("fight")
